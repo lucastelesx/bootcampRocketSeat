e074d1d27f09d942d26f21d278001ebdf05c6bd8
lucastelesfinger <lucas.teles@fingergital.com.br>
2019-08-10 15:38:15 -0300

    add migration to create user updated routes to create user

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'hello manoaaaa' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'lucas teles',
+    email: 'lucas_telles25@hotmail.com',
+    password_hash: 'coelho5552',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
